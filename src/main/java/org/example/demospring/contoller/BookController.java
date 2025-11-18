58cd08dfd91ba65b6f18905d9f82670d5819d5ed
Oleh Ridchenko <olehridchenko@gmail.com>
2025-11-18 21:54:57 +0200

    change openapi version and add interface with all method and anotations

--- src/main/java/org/example/demospring/contoller/BookController.java
+++ src/main/java/org/example/demospring/contoller/BookController.java
@@ -1,71 +1,50 @@
 package org.example.demospring.contoller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import java.util.List;
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.example.demospring.dto.BookDto;
 import org.example.demospring.dto.BookSearchParameters;
 import org.example.demospring.dto.CreateBookRequestDto;
 import org.example.demospring.service.BookService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Book store management", description = "Endpoints for all books")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/books")
-public class BookController {
+public class BookController implements BookControllerApi{
     private final BookService bookService;
 
-    @Operation(summary = "Get all books")
-    @GetMapping
+    @Override
     public Page<BookDto> findAll(Pageable pageable) {
         return bookService.findAll(pageable);
     }
 
-    @Operation(summary = "Get book with some id")
-    @GetMapping("/{id}")
-    public BookDto getBookById(@PathVariable Long id) {
+    @Override
+    public BookDto getBookById(Long id) {
         return bookService.findById(id);
     }
 
-    @Operation(summary = "Crate a new book")
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    public BookDto createBook(@Valid
-                                  @RequestBody CreateBookRequestDto requestDto) {
+    @Override
+    public BookDto createBook(CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
 
-    @Operation(summary = "Update existing book")
-    @PutMapping("/{id}")
-    public BookDto updateBookById(@PathVariable Long id,
-                                  @Valid
-                                  @RequestBody CreateBookRequestDto bookRequestDto) {
+    @Override
+    public BookDto updateBookById(Long id, CreateBookRequestDto bookRequestDto) {
         return bookService.updateBookById(id, bookRequestDto);
     }
 
-    @Operation(summary = "Remove existing book by some id")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable Long id) {
+    @Override
+    public void deleteById(Long id) {
         bookService.deleteById(id);
     }
 
-    @Operation(summary = "Find book by some parameters")
-    @GetMapping("/search")
+    @Override
     public List<BookDto> searchBooks(BookSearchParameters searchParameters) {
         return bookService.search(searchParameters);
     }
