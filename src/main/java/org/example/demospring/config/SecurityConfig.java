c9132a3a80d6079c7e3c553e3f1da38e66375e1d
Oleh Ridchenko <olehridchenko@gmail.com>
2025-12-05 12:38:01 +0200

    add secret and expiration to test app properties

--- src/main/java/org/example/demospring/config/SecurityConfig.java
+++ src/main/java/org/example/demospring/config/SecurityConfig.java
@@ -20,7 +20,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableMethodSecurity
 @Configuration
 @RequiredArgsConstructor
-public class    SecurityConfig {
+public class SecurityConfig {
     private final UserDetailsService userDetailsService;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
