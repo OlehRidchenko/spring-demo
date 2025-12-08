3c4a6e888dd8fc0f82bfdacd2d6dd64976cb2935
Oleh Ridchenko <olehridchenko@gmail.com>
2025-12-08 13:36:24 +0200

    add swagger docs to auth conroller

--- src/main/java/org/example/demospring/contoller/AuthenticationController.java
+++ src/main/java/org/example/demospring/contoller/AuthenticationController.java
@@ -1,5 +1,7 @@
 package org.example.demospring.contoller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.demospring.dto.user.UserLoginRequestDto;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "User authentication management", description = "Endpoints register, login for user with role")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
@@ -23,12 +26,14 @@ public class AuthenticationController {
     private final UserService userService;
     private final AuthenticationService authenticationService;
 
+    @Operation(summary = "Public endpoint for registration")
     @PostMapping("/registration")
     @ResponseStatus(HttpStatus.CREATED)
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto request) throws RegistrationException {
         return userService.register(request);
     }
 
+    @Operation(summary = "Public endpoint for logging in")
     @PostMapping("/login")
     public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto requestDto) {
         return authenticationService.authenticate(requestDto);
